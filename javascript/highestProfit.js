0964667635b374210f80647cff68fbfeea219ca2
hfvos90 <hfvos90@gmail.com>
2019-04-24 12:51:22 -0700

    new changes

---
+++ javascript/highestProfit.js
@@ -0,0 +1,23 @@
+function minMax(arr){
+  //min
+  let min = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  //max
+  let max = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+  return [min,max]; // fix me!
+}
+
+
+console.log(minMax([1,2,3,4,5]));
+console.log(minMax([2334454,5]));
+// [ 1, 5 ]
+// [ 5, 2334454 ]

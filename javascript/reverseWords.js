6abe5dc759605a7f786b1f01791ad6d07c6f45d7
hfvos90 <hfvos90@gmail.com>
2019-04-24 13:41:16 -0700

    reverseWords

---
+++ javascript/reverseWords.js
@@ -0,0 +1,23 @@
+function reverseWords(str) {
+  // Go for it
+  let complete = []
+  let stuff = str.split(' ')
+  console.log(stuff)
+  // [ 'This', 'is', 'an', 'example' ]
+  for (var i = 0; i < stuff.length; i++) {
+    // console.log(stuff[i].split(''))
+    // [ 'T', 'h', 'i', 's' ]
+    // [ 'i', 's' ]
+    // [ 'a', 'n' ]
+    // [ 'e', 'x', 'a', 'm', 'p', 'l', 'e' ]
+    console.log(stuff[i].split('').reverse().join(''))
+    // sihT
+    // si
+    // na
+    // elpmaxe
+    complete.push(stuff[i].split('').reverse().join(''))
+  }
+  return complete.join(' ')
+}
+console.log(reverseWords("This is an example"));
+// sihT si na elpmaxe
